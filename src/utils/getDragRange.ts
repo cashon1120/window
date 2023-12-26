7cbe34fdbb6bc756211369c956d4ed6ea499a7eb
cashon1120 <49798613@qq.com>
2023-12-26 16:00:50 +0800

    getDragRange bug修复

--- src/utils/getDragRange.ts
+++ src/utils/getDragRange.ts
@@ -4,7 +4,7 @@ import { BarProps } from "@/types";
  * 拖动前查询能拖动的范围
  */
 export default (props: BarProps) => {
-  const { name, params, data } = props;
+  const { name, params, data, type } = props;
   let currentMin = 0;
   let currentMax = 0;
   const result = {
@@ -25,18 +25,21 @@ export default (props: BarProps) => {
       break;
   }
   // 拿到当前模型所在的位置
+  // top / left 的位置
   currentMin = params[positionKey];
+  // right / bottom 的位置
   currentMax = params[positionKey] + params[sizeKey];
   Object.keys(data).forEach((key) => {
     const { attribute } = data[key];
-    if (key !== name) {
-      if (attribute[positionKey] + attribute[sizeKey] < currentMin) {
+    // 判断相同 type 的模型，目前只有 Bar 的两种类型(vertical / horizontal),后期还会涉及到 Rect 模型的判断
+    if (key !== name && type === data[key].type) {
+      if (attribute[positionKey] + attribute[sizeKey] <= currentMin) {
         result.min = Math.max(
           result.min,
           attribute[positionKey] + attribute[sizeKey]
         );
       }
-      if (attribute[positionKey] > currentMax) {
+      if (attribute[positionKey] >= currentMax) {
         result.max = Math.min(
           result.max,
           attribute[positionKey] - params[sizeKey]
