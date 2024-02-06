767b25d6c75fc21e45390377ed0d24ee141674ca
cashon1120 <49798613@qq.com>
2024-02-06 15:20:27 +0800

    细节调整

--- src/threeD/index.ts
+++ src/threeD/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as THREE from "three";
 import * as Models from "./models";
 import {
@@ -22,7 +23,13 @@ interface Params {
 }
 
 // 所有模型的类
-export type Model = BottomFrame | TopFrame | LeftFrame | RightFrame | NormalBar | Window;
+export type Model =
+  | BottomFrame
+  | TopFrame
+  | LeftFrame
+  | RightFrame
+  | NormalBar
+  | Window;
 
 export interface ThreeDObject {
   [key: string]: Model;
@@ -90,3 +97,49 @@ const reset3D = () => {
 };
 
 export { init3D, reset3D };
+
+const arr = [
+  {
+    id: "1717156645471133698",
+    metadataName: "玻扇",
+    objectName: "glassFan",
+    fieldId: 276,
+    fieldName: "handle",
+    describe: "执手",
+    dataType: 2,
+    metadataEnglishName: "glassFan_handle",
+    metadataChineseName: "玻扇执手",
+    useCount: 0,
+    lastTime: "-31557014135625943",
+  },
+  {
+    id: "1717156645471133698",
+    metadataName: "玻扇",
+    objectName: "glassFan",
+    fieldId: 279,
+    fieldName: "handleDrift",
+    describe: "执手偏移",
+    dataType: 2,
+    metadataEnglishName: "glassFan_handleDrift",
+    metadataChineseName: "玻扇执手偏移",
+    useCount: 0,
+    lastTime: "-31557014135625943",
+  },
+];
+
+const getEnglishName = (str: string, arr: any[]) => {
+  const keyValues: any = {};
+  arr.forEach((item: any) => {
+    keyValues[item.metadataChineseName] = item.metadataEnglishName;
+  });
+  const valueList = str.match(/[\u4e00-\u9fa5\d]+/g);
+  const symbolList = str.match(/[^\u4e00-\u9fa5\d]/g);
+  if(valueList && symbolList){
+    return valueList.reduce((prev: string, cur: string, index: number) => prev + (keyValues[cur] || cur) + (symbolList[index] || ""), "");
+  }
+};
+
+const str = "玻扇执手>玻扇执手偏移+100=玻扇面积x20";
+console.log(getEnglishName(str, arr));
+// 输入: 玻扇执手         > 玻扇执手偏移           + 100 = 玻扇面积 x 20
+// 输出: glassFan_handle > glassFan_handleDrift + 100 = 玻扇面积 x 20
